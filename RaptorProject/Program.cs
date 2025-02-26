3d2a3a0ffe436c9056ef72925a539c2e363d59f4
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-02-26 15:12:56 -0700

    Add project files.

---
+++ RaptorProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Spark;
+using Microsoft.Spark.Sql;
+
+namespace RaptorProject;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
