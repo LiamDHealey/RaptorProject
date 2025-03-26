122be71b2148d1e96364f6e428924c0f81e43011
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-03-26 13:15:02 -0600

    Yay

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ wait
   --master local[4] \
   --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=custom-log4j.properties"\
   --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=custom-log4j.properties"\
-  target/scala-2.12/simple-project_2.12-1.0.jar
+  target/scala-2.12/simple-project_2.12-1.0.jar > spark.log
