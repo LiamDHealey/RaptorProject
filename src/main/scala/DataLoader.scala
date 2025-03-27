6c23e983777871d84bfa1552a3979fa740d14fec
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-03-27 12:15:32 -0600

    Some cleanup

--- src/main/scala/DataLoader.scala
+++ src/main/scala/DataLoader.scala
@@ -2,21 +2,26 @@ import org.apache.spark.rdd.RDD
 import javax.imageio.ImageIO
 import java.io.File
 import org.apache.spark.sql._
-import Main._
+import Main.spark
 import spark.implicits._
 
 object DataLoader {
     def getRasterData(): Dataset[Pixel] = {
         val ds = spark.emptyDataset[Pixel]
 
-        new File("data/Raster").listFiles().foreach(file => {
+        new File("data/Test").listFiles().foreach(file => {
             val image = ImageIO.read(file)
-            val xOffset = Integer.parseInt(file.getName().substring(9,  11)) * 10812
-            val yOffset = Integer.parseInt(file.getName().substring(12, 15)) * 10812
+
+            val width = image.getWidth()
+            val hieght = image.getHeight()
+            val xOffset = Integer.parseInt(file.getName().substring(9,  11)) * width
+            val yOffset = Integer.parseInt(file.getName().substring(12, 15)) * hieght
+
             val data = (
-                (0 until image.getWidth(), 0 until image.getHeight())
+                (0 until width, 0 until hieght)
                 .zipped.map((x, y) => Pixel(x + xOffset, y + yOffset, image.getRGB(x, y)))
             ).toDS()
+
             ds.union(data)
         })
         
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,23 +3,17 @@ import org.apache.spark.sql.types._
 
 object Main {  
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-
-    val rasterDataScema = StructType(List(
-        StructField("x", IntegerType, false),
-        StructField("y", IntegerType, false),
-        StructField("elevation", IntegerType, true),
-    ))
-    val vectorDataScema = StructType(List(
-        StructField("x", IntegerType, false),
-        StructField("y", IntegerType, false),
-        StructField("elevation", IntegerType, true),
-    ))
     
     def main(args: Array[String]): Unit = {
+        println("Start Loading")
+
         // Load Data
         val imageData = DataLoader.getRasterData()
-        val vectorData = DataLoader.getVectorData()
+        println("Done Loading")
         
+        imageData.show()
+        //val vectorData = DataLoader.getVectorData()
+
         // Stop Spark
         spark.stop()
     }
