b3e681c74cf789d7ee2935aa64ce6f7f0f0aba14
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-03-27 11:51:06 -0600

    Fix run script

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ wait
 sbt package
 wait
 /opt/spark/bin/spark-submit \
-  --class "SimpleApp" \
+  --class "Main" \
   --master local[4] \
   --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=custom-log4j.properties"\
   --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=custom-log4j.properties"\
-  target/scala-2.12/simple-project_2.12-1.0.jar > output.log
+  target/scala-2.12/raptor-project_2.12-1.0.jar > output.log
 wait 
 clear
 cat output.log
