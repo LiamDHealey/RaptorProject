a87e8b75ed53fd2ed400512235a7893e45a89e52
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-04-25 12:00:08 -0600

    Work on point in polygon

--- src/main/scala/ComputationMethod.scala
+++ src/main/scala/ComputationMethod.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.sql.Dataset
 
 
-final case class Result(state: String, county: String, averageElevation: Float)
+final case class Result(state: String, county: String, averageElevation: Double)
 
 trait ComputationMethod {
   def compute(rasterData: Dataset[Pixel], vectorData: Dataset[Region]): Dataset[Result]
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,17 +10,29 @@ object Main {
         println("Start Loading")
         // Image data is a dataset where x & y are mesured in 1/3 arc-seconds
         val imageData = RasterLoader.getRasterData()
-        imageData.show(10)
-        println(s"count: ${imageData.count()}")
-        // val vectorData = VectorLoader.getVectorData()
-        // vectorData.show(10)
+        val vectorData = VectorLoader.getVectorData()
         println("Done Loading")
-        println("Aggregating QuadTree")
-        // val AggregateQuadTreeResult = AggregateQuadTree.compute(imageData, vectorData)
-        // AggregateQuadTreeResult.show(10)
-        println("QuadTree Done")
 
-        //PointInPolygon.compute(imageData, vectorData).foreach(println)
+
+        println("Point in Polygon")
+        val PointInPolygonReult = PointInPolygon.compute(imageData, vectorData)
+        PointInPolygonReult.show(10)
+        println("Point in Polygon Done")
+
+
+        //println("Clipping")
+        //val ClippingResult = Clipping.compute(imageData, vectorData)
+        //ClippingResult.show(10)
+        //println("Clipping Done")
+
+
+        //println("Aggregating QuadTree")
+        //val AggregateQuadTreeResult = AggregateQuadTree.compute(imageData, vectorData)
+        //AggregateQuadTreeResult.show(10)
+        //println("QuadTree Done")
+
+
+
 
         // Stop Spark
         spark.stop()
