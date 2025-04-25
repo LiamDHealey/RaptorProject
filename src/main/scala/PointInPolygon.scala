cc3c62e618ce92ed3b24f71ffa7c4cb425075ad3
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-04-25 12:25:18 -0600

    Filter for utah

--- src/main/scala/PointInPolygon.scala
+++ src/main/scala/PointInPolygon.scala
@@ -5,14 +5,14 @@ import spark.implicits._
 
 object PointInPolygon extends ComputationMethod {
   override def compute(rasterData: Dataset[Pixel], vectorData: Dataset[Region]): Dataset[Result] = {
-    return spark.createDataset(vectorData.collect().map(region => {
-        println(s"   PointInPoly: ${region.county}, ${region.state}")
+    val utahOnly = vectorData.filter(r => r.state == "Utah")
+    return spark.createDataset(utahOnly.collect().map(region => {
+        println(s"   Starting PointInPoly: ${region.county}, ${region.state}")
         val averageElevation = rasterData.filter(pixel => {
             true
         }).agg("elevation" -> "avg")
         .collect()(0)
         .getAs[Double](0)
-        
 
         new Result(region.state, region.county, averageElevation)
     }))
