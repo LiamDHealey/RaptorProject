a58e5798385fbb26bf535dfd18efddecfc58f8c3
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-04-29 12:45:19 -0600

    Timeing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,36 +11,45 @@ object Main {
         // Image data is a dataset where x & y are mesured in arc-seconds
         val imageData = RasterLoader.getRasterData()
         imageData.show(5, truncate = false)
-        
         val total = imageData.count()
         println(s"Total number of pixels: $total")
+
         val vectorData = VectorLoader.getVectorData()
         vectorData.show(10)
         
         println("Done Loading")
-        
-        
+        var startTime = 0L
+        var duration = 0.0
         println("Point in Polygon")
-        val PointInPolygonReult = PointInPolygon.compute(imageData.sample(0.0000001), vectorData)
-        PointInPolygonReult.show(10)
-        println("Point in Polygon Done")
+        startTime = System.nanoTime
+        val pointInPolySampleFraction = 0.00001
+        val pointInPolygonReult = PointInPolygon.compute(imageData.sample(pointInPolySampleFraction), vectorData)
+        pointInPolygonReult.show(50)
+        duration = (System.nanoTime - startTime) / 1e9d / pointInPolySampleFraction
+        println(s"Point in Polygon Done | Took $duration")
         
         
-        // println("Clipping")
-        // //Need specific lat and lon values for clipping
-        // //LLRasterLoader incluides those values when parsing
-        // val LLimageData = LLRasterLoader.getRasterData()
-        // LLimageData.show(5, truncate = false)
-        // val sampledRasterData = LLimageData.sample(withReplacement = false, fraction = 0.00001) // 0.001% of pixels
-        // val ClippingResult = Clipping.clipCompute(sampledRasterData, vectorData)
-        // ClippingResult.show(10)
-        // println("Clipping Done")
+        println("Clipping")
+        //Need specific lat and lon values for clipping
+        //LLRasterLoader incluides those values when parsing
+        val LLimageData = LLRasterLoader.getRasterData()
+        LLimageData.show(5, truncate = false)
+
+        startTime = System.nanoTime
+        val clippingSampleFraction = 0.00001
+        val sampledRasterData = LLimageData.sample(withReplacement = false, fraction = 0.00001) // 0.001% of pixels
+        val ClippingResult = Clipping.clipCompute(sampledRasterData, vectorData)
+        ClippingResult.show(50)
+        duration = (System.nanoTime - startTime) / 1e9d / clippingSampleFraction
+        println(s"Clipping Done | Took $duration")
 
 
-        // println("Aggregating QuadTree")
-        // val AggregateQuadTreeResult = AggregateQuadTree.compute(imageData, vectorData)
-        // AggregateQuadTreeResult.show(29)
-        // println("QuadTree Done")
+        println("Aggregating QuadTree")
+        startTime = System.nanoTime
+        val AggregateQuadTreeResult = AggregateQuadTree.compute(imageData, vectorData)
+        AggregateQuadTreeResult.show(29)
+        duration = (System.nanoTime - startTime) / 1e9d
+        println(s"QuadTree Done | Took $duration")
 
 
 
